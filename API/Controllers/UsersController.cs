bc2560bd18a3511ca01f3dd5972c0d649cc12995
Charles Showalter <cshowalter@xtechnology.org>
2022-06-03 14:47:59 -0700

    Created BaseApiController.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,12 +1,11 @@
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
