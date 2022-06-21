22efde0f41e0878c90fff270a19a7c991320709b
Charles Showalter <cshowalter@xtechnology.org>
2022-06-21 14:06:48 -0700

    Added Routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 import {User} from "../_models/users";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ import {User} from "../_models/users";
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +20,7 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe({
       next:(response) => {
-        console.log(response)
+        this.router.navigateByUrl('/members');
       },
       error:(e: any) => { console.log(e) },
       complete: () => { console.log("completed") }
@@ -27,6 +28,7 @@ export class NavComponent implements OnInit {
   }
 
   logout(){
-    this.accountService.logout()
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
